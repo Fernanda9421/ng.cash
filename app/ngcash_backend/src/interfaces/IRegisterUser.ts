264b9cffd1131b91e66f0ca589c15206fcaa46f0
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-11-16 10:48:34 +0000

    feat: cria interface de Login

---
+++ app/ngcash_backend/src/interfaces/IRegisterUser.ts
@@ -0,0 +1,14 @@
+export interface ILogin {
+  username: string;
+  password: string;
+}
+
+// export interface IRegister extends ILogin {
+//   accountId?: number;
+//   status?: number;
+// }
+
+// export interface IUser extends ILogin {
+//   id: number,
+//   accountId: number
+// }
