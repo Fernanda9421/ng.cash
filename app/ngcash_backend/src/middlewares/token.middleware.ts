0e08a7c8d212e5bae663c84d48ded38ad9032088
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-11-16 21:49:52 +0000

    feat: cria middleware de validação de token

---
+++ app/ngcash_backend/src/middlewares/token.middleware.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+import { secret } from '../database/config/jwt.config';
+import HttpException from '../exceptions/HttpException';
+
+export function validateToken(req:Request, _res:Response, next:NextFunction) {
+  const { authorization: token } = req.headers;
+
+  if (!token || token === '') throw new HttpException(401, 'Token not found');
+
+  try {
+    verify(token, secret);
+
+    next();
+  } catch (error) {
+    console.log(error);
+    throw new HttpException(401, 'Expired or invalid token');
+  }
+}
