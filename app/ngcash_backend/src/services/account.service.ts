181a1ac5baad1fa45e74fc01667313702d761923
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-11-16 20:19:34 +0000

    refactor: altera função para classe

--- app/ngcash_backend/src/services/account.service.ts
+++ app/ngcash_backend/src/services/account.service.ts
@@ -2,21 +2,10 @@ import Account from '../database/models/Account';
 
 const INITIAL_BALANCE = '100,00';
 
-export async function createAccount() {
-  const account = await Account.create({ balance: INITIAL_BALANCE });
-  console.log(account);
+export class AccountService {
+  public async createAccount():Promise<Account> {
+    const account = await Account.create({ balance: INITIAL_BALANCE });
 
-  return { status: 201, account };
+    return account;
+  }
 }
-
-// class AccountService {
-//   static createAccount = async ():Promise<any | null> => {
-//     const account = await Account.create({
-//       balance: INITIAL_BALANCE,
-//     });
-
-//     return account.dataValues.id;
-//   };
-// }
-
-// export default AccountService;
