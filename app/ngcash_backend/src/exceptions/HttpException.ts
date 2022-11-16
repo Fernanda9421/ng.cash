e9df7f1950129f77b36881cd8e07705c7c617be3
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-11-16 10:42:33 +0000

    feat: cria exceção de erro

---
+++ app/ngcash_backend/src/exceptions/HttpException.ts
@@ -0,0 +1,12 @@
+class HttpException extends Error {
+  declare status: number;
+  declare message: string;
+
+  constructor(status: number, message: string) {
+    super(message);
+    this.status = status;
+    this.message = message;
+  }
+}
+
+export default HttpException;
