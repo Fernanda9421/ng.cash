d551402c5b0bd20a27f89b2befe484bae31ab303
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-11-18 09:03:03 +0000

    feat: busca usuários envolvidos na transação

--- app/ngcash_backend/src/services/user.service.ts
+++ app/ngcash_backend/src/services/user.service.ts
@@ -5,6 +5,9 @@ import { IUser } from '../interfaces/INewUser';
 import { encryptPassword } from '../utils/encryptPassword';
 import { tokenGenerate } from '../utils/tokenGenerate';
 import { AccountService } from './account.service';
+import Account from '../database/models/Account';
+import { isValidAccount } from '../utils/validAccount';
+import { IUsersTransaction } from '../interfaces/IUserTransaction';
 
 export class UserService {
   private accountService: AccountService;
@@ -14,7 +17,14 @@ export class UserService {
   }
 
   public async getByUsername(username:string):Promise<User | null> {
-    const user = await User.findOne({ where: { username } });
+    const user = await User.findOne({
+      where: { username },
+      include: [
+        {
+          model: Account,
+        },
+      ],
+    });
 
     if (!user) return null;
     return user;
@@ -36,6 +46,34 @@ export class UserService {
     return {
       newUser,
       token,
+    };
+  }
+
+  public async getUserById(id:number):Promise<User | void> {
+    const user = await User.findByPk(id, {
+      include: [
+        {
+          model: Account,
+        },
+      ],
+    });
+
+    if (!user) throw new HttpException(404, 'User not found');
+    return user;
+  }
+
+  public async getUsersTransaction(idCashOut:string, usernameCashIn:string):Promise<IUsersTransaction> {
+    const userCashOut = await this.getUserById(Number(idCashOut));
+    const userCashIn = await this.getByUsername(usernameCashIn);
+
+    if (!userCashOut || !userCashIn) throw new HttpException(400, 'Invalid username');
+
+    const validAccount = isValidAccount(userCashIn.dataValues.id, userCashOut.dataValues.id);
+    if (!validAccount) throw new HttpException(400, 'Invalid username');
+
+    return {
+      cashOut: userCashOut,
+      cashIn: userCashIn,
     };
   }
 }
