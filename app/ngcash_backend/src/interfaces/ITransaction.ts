9a107b349c370cf865996626d7f49889ddf72139
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-11-18 10:30:49 +0000

    feat: cria interface para transações retornadas

--- app/ngcash_backend/src/interfaces/ITransaction.ts
+++ app/ngcash_backend/src/interfaces/ITransaction.ts
@@ -1,4 +1,11 @@
+import Transaction from '../database/models/Transaction';
+
 export interface IRequestTransaction {
   username: string;
   value: number;
+}
+
+export interface ITransactions {
+  cashOut: Transaction[],
+  cashIn: Transaction[],
 }
