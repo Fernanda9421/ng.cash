5a3e365b5569588a3d279686c979352cdce5914c
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-11-20 10:49:14 +0000

    refactor: atualiza gerador e verificador de token

--- app/ngcash_backend/src/utils/tokenGenerate.ts
+++ app/ngcash_backend/src/utils/tokenGenerate.ts
@@ -1,6 +1,8 @@
 import jwt from 'jsonwebtoken';
 import { secret, options } from '../database/config/jwt.config';
+import HttpException from '../exceptions/HttpException';
 
 export function tokenGenerate(username:string, id:number):string {
+  if (secret === '') throw new HttpException(400, 'The secret cannot be empty');
   return jwt.sign({ username, id }, secret, options);
 }
