4d83aa05695e68a72c26e44c1730706f5d002511
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-11-21 14:23:34 +0000

    feat: cria helper com mensagens de erro

---
+++ app/ngcash_backend/tests/helpers/errorMessage.ts
@@ -0,0 +1,10 @@
+export const ERROR = {
+  ALREADY_EXISTS: 'User already exists',
+  INVALID_USERNAME: 'Username must be at least 3 characters long!',
+  INVALID_PASSWORD: 'Password must be at least 8 characters long, a number and an uppercase letter!',
+  INVALID_FIELDS: 'Invalid fields',
+  TOKEN_NOT_FOUND: 'Token not found',
+  INVALID_TOKEN: 'Expired or invalid token',
+  INVALID_BALANCE: 'Invalid balance',
+  INVALID_USER: 'Invalid User',
+};
