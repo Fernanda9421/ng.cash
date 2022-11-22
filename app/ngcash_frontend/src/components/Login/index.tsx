a7d07f23ef73bce57962527c55e3e161778fa6d4
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-11-22 15:45:48 +0000

    feat: adiciona funcionalidade de logar usuário

--- app/ngcash_frontend/src/components/Login/index.tsx
+++ app/ngcash_frontend/src/components/Login/index.tsx
@@ -1,12 +1,29 @@
-import Image from 'next/image';
 import LoginImage from '../../images/login.png';
 import styles from '@/styles/Form/heroForm.module.css';
 import Form from '../Form';
 import { IFormInputs } from '../InputField/interfaces';
 import HeroForm from '../HeroForm';
+import { useContext } from 'react';
+import { AppContext } from 'src/context/AppContext';
+import { requestPost } from 'src/services/requests';
+import { useRouter } from 'next/router';
 
 const Login = () => {
-  const onSubmit = (data:IFormInputs) => console.log(data);
+  const { setError } = useContext(AppContext);
+  const route = useRouter();
+
+  const onSubmit = async (data:IFormInputs) => {
+    const endpoint = '/login';
+
+    try {
+      await requestPost(endpoint, data);
+      setError('');
+      route.push('/account');
+    } catch (error:any) {
+      console.log(error.response.data.message);
+      setError(error.response.data.message);
+    }
+  };
 
   return (
     <div className={styles.container}>
