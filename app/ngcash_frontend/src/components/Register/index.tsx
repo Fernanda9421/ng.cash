daa38165a7601701c026fef2617c06fa9916dae3
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-11-22 15:45:23 +0000

    feat: adiciona funcionalidade de criar usuário

--- app/ngcash_frontend/src/components/Register/index.tsx
+++ app/ngcash_frontend/src/components/Register/index.tsx
@@ -1,12 +1,28 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useContext } from 'react';
 import Form from '../Form';
 import { IFormInputs } from '../InputField/interfaces';
 import RegisterImage from '../../images/register.png';
 import styles from '@/styles/Form/heroForm.module.css';
 import HeroForm from '../HeroForm';
+import { requestPost } from 'src/services/requests';
+import { AppContext } from 'src/context/AppContext';
+import { useRouter } from 'next/router';
 
 const Register: FunctionComponent = () => {
-  const onSubmit = (data:IFormInputs) => console.log(data);
+  const { setError } = useContext(AppContext);
+  const route = useRouter();
+  const onSubmit = async (data:IFormInputs) => {
+    const endpoint = '/register';
+
+    try {
+      await requestPost(endpoint, data);
+      setError('');
+      route.push('/account');
+    } catch (error:any) {
+      console.log(error.response.data.message);
+      setError(error.response.data.message);
+    }
+  };
 
   return (
     <div className={styles.container}>
