e529c243bb5790cae9eba7c22433e69eb6d9e04d
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-11-23 21:27:17 +0000

    feat: página de realizar transferências

---
+++ app/ngcash_frontend/src/pages/transfer.tsx
@@ -0,0 +1,14 @@
+import Header from '@/components/Header';
+import Transfer from '@/components/Transfer';
+import styles from '@/styles/Transfer/transfer.module.css';
+
+const TransferMoney = () => {
+  return (
+    <div className={styles.container}>
+      <Header />
+      <Transfer />
+    </div>
+  );
+};
+
+export default TransferMoney;
