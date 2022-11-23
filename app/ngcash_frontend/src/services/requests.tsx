2683963c530c62f6cdfa2230de78c8b4ecb82e11
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-11-23 10:57:43 +0000

    feat: requisição GET

--- app/ngcash_frontend/src/services/requests.tsx
+++ app/ngcash_frontend/src/services/requests.tsx
@@ -4,11 +4,20 @@ import { IBodyForm } from './interfaces';
 const port = process.env.REACT_APP_BACKEND_PORT || '3001';
 const baseURL = process.env.REACT_APP_BACKEND_BASE_URL || 'http://localhost';
 
-const api = axios.create({
-  baseURL: `${baseURL}:${port}`,
+export const api = axios.create({
+  baseURL: `${baseURL}:${port}`
 });
 
-export const requestPost= async (endpoint:string, body:IBodyForm) => {
+export const setToken = (token:string) => {
+  api.defaults.headers.common['Authorization'] = token;
+};
+
+export const requestPost = async (endpoint:string, body:IBodyForm) => {
   const { data } = await api.post(endpoint, body);
+  return data;
+};
+
+export const requestGet = async (endpoint:string) => {
+  const { data } = await api.get(endpoint);
   return data;
 };
