77521226443e6b94697b476f19b6e45c95bd4da6
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-11-23 10:58:01 +0000

    feat: localstorage getItem

--- app/ngcash_frontend/src/utils/localStorage.tsx
+++ app/ngcash_frontend/src/utils/localStorage.tsx
@@ -1,8 +1,16 @@
 interface Value {
   token: string;
   username: string;
+  id: number;
 }
 
 export const storageSetItem = (key:string, value:Value) => {
   return localStorage.setItem(key, JSON.stringify(value));
+};
+
+export const storageGetItem = (key:string) => {
+  if (typeof window !== 'undefined') {
+    const item = localStorage.getItem(key);
+    if (item !== null) return JSON.parse(item);
+  }
 };
