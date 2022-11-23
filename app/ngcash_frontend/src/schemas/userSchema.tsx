6c3ff094a581da88ca661e45f0e43d654520ec3f
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-11-23 21:24:26 +0000

    feat: adiciona schema de transferência

--- app/ngcash_frontend/src/schemas/userSchema.tsx
+++ app/ngcash_frontend/src/schemas/userSchema.tsx
@@ -6,6 +6,7 @@ const errors = {
   blank: 'Campo é obrigatório',
   invalidUsername: 'Mínimo 3 caracteres',
   invalidPassword: 'Mínimo 8 caracteres, 1 número e 1 letra maiúscula',
+  invalidValue: 'Valor deve ser maior que 0',
 };
 
 export const userSchema = yup.object({
@@ -14,3 +15,8 @@ export const userSchema = yup.object({
     .minUppercase(1, errors.invalidPassword).minNumbers(1, errors.invalidPassword)
     .min(8, errors.invalidPassword).required(errors.blank),
 }).required();
+
+export const cashInSchema = yup.object({
+  username: yup.string().min(3, errors.invalidUsername).required(errors.blank),
+  value: yup.number().min(0.01, errors.invalidValue).required(errors.blank),
+});
