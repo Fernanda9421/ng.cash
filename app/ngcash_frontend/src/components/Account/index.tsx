7df84b7c93a3824d89d123e2880a3d96cfad8412
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-11-23 11:01:03 +0000

    feat: cria componente Acount, com informações de conta do usuário

---
+++ app/ngcash_frontend/src/components/Account/index.tsx
@@ -0,0 +1,63 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { Exception } from 'src/interfaces/error';
+import { api, requestGet, setToken } from 'src/services/requests';
+import { storageGetItem } from 'src/utils/localStorage';
+import styles from '@/styles/Account/account.module.css';
+import Link from 'next/link';
+import Button from '../Button';
+
+// const redirect = (route:any) => route.push('/');
+
+const Account = () => {
+  const route = useRouter();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState('');
+  const [balance, setBalance] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      const { token, id, username } = storageGetItem('user');
+  
+      if (!token) route.push('/');
+      setToken(token);
+      setUser(username);
+  
+      try {
+        const account = await requestGet(`/account/${id}`);
+        setIsAuthenticated(true);
+        setBalance(account.balance);
+      } catch (error) {
+        const result = (error as Exception).response.data.message;
+        console.log(result);
+        setIsAuthenticated(false);
+        route.push('/');
+      }
+    })(); 
+  }, []);
+
+  return (
+    <div className={styles.presentationContainer}>
+      {
+        isAuthenticated && (
+          <div className={styles.box}>
+            <div className={styles.user}>
+              <span className={styles.username}>@{user}</span>
+              <span className={styles.welcome}>Boas-vindas</span>
+            </div>
+            <div className={styles.balance}>
+              <span className='text-secondary mr-3'>Saldo atual:</span>
+              <span>R${balance}</span>
+            </div>
+            <div className='flex my-2'>
+              <Button className={styles.button} name='Operações' />
+              <Button className={styles.button} name='Transferir' />
+            </div>
+          </div>
+        )
+      }
+    </div>
+  );
+};
+
+export default Account;
