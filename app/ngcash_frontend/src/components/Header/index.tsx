22b1631d4a1d398999fa990c79a6ccd1c17a1aec
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-11-24 15:41:17 +0000

    feat: adiciona link ao logotipo

--- app/ngcash_frontend/src/components/Header/index.tsx
+++ app/ngcash_frontend/src/components/Header/index.tsx
@@ -6,7 +6,9 @@ import styles from '@/styles/Header/header.module.css';
 const Header: FunctionComponent = () => {
   return (
     <div className={styles.container}>
-      <p className={styles.logo}>NG.CA$H</p>
+      <Link href='/account' className={styles.logo}>
+        NG.CA$H
+      </Link>
       <Link href='/' className={styles.signOut}>
         Sair <FaSignOutAlt />
       </Link>
