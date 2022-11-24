7f160aea92613cd3d2b2f1606ce5f138f53a0b06
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-11-24 09:32:03 +0000

    feat: cria componente de erro na transação

---
+++ app/ngcash_frontend/src/components/ErrorTransfer/index.tsx
@@ -0,0 +1,42 @@
+import { useContext } from 'react';
+import styles from '@/styles/Transfer/transferStatus.module.css';
+import { useRouter } from 'next/router';
+import Button from '../Button';
+import { AppContext } from 'src/context/AppContext';
+import { IError } from '../SuccessTransfer/interfaces';
+import Image from 'next/image';
+import ErrorImage from '../../images/error.png';
+
+const ErrorTransfer = ({ setIsTransactionSuccess }:IError) => {
+  const route = useRouter();
+  const { error, setError } = useContext(AppContext);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.box}>
+        <h1 className={styles.title}>Ops! Algo deu errado com sua transferência.</h1>
+        <div className={styles.receipt}>
+          <Image
+            src={ErrorImage}
+            alt='error'
+            width={100}
+            height={300}
+          />
+          {
+            error === 'Invalid fields' ? (
+              <p>O nome de usuário informado é inválido</p>
+            ) : (
+              <p>Parece que você não tem saldo suficiente para realizar essa transação :( </p>
+            )
+          }
+        </div>
+        <div className={styles.buttons}>
+          <Button className={styles.button} name='Nova transferência' onClick={ () => { setError(''); setIsTransactionSuccess(false); } } />
+          <Button className={styles.button} name='Página principal' onClick={ () => { setError(''); route.push('/account'); } }/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorTransfer;
--- app/ngcash_frontend/src/components/SuccessTransfer/interfaces/index.tsx
+++ app/ngcash_frontend/src/components/SuccessTransfer/interfaces/index.tsx
@@ -1,10 +1,14 @@
 import { Dispatch, SetStateAction } from 'react';
 
 type ITransactionSuccess = boolean;
+type IErrorTransaction = boolean;
 
-export interface ISuccess {
+export interface IError {
+  setIsTransactionSuccess: Dispatch<SetStateAction<ITransactionSuccess>> | Dispatch<SetStateAction<IErrorTransaction>>,
+}
+
+export interface ISuccess extends IError {
   user: string,
   value: number,
   date: string,
-  setIsTransactionSuccess: Dispatch<SetStateAction<ITransactionSuccess>>,
 }
