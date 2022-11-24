ef4530bbe26e5742ff545ce01e8bbcda5dbf296f
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-11-24 15:41:41 +0000

    feat: cria tabela de transações

---
+++ app/ngcash_frontend/src/components/TransactionTable/index.tsx
@@ -0,0 +1,63 @@
+import { FunctionComponent, useEffect, useState } from 'react';
+import { storageGetItem } from 'src/utils/localStorage';
+import styles from '@/styles/TransactionTable/transactionTable.module.css';
+import { convertValue } from 'src/utils/convertValue';
+import { convertDate } from 'src/utils/convertData';
+import { ITableTransaction, ITransaction } from '../Operation/interfaces';
+
+const TransactionTable = (
+  {transactions, title, head1, head2, head3, type}:ITableTransaction
+) => {
+  const [userId, setUserId] = useState(0);
+
+  useEffect(() => {
+    const { id } = storageGetItem('user');
+    setUserId(id);
+  }, []);
+
+  const credited = () => {
+    return transactions.filter((transaction:ITransaction) => transaction.creditedAccountId === userId);
+  };
+
+  const debited = () => {
+    return transactions.filter((transaction:ITransaction) => transaction.debitedAccountId === userId);
+  };
+
+  return (
+    <div className={styles.tableContainer}>
+      <h1 className={styles.title}>{title}</h1>
+      <table className={styles.table}>
+        <thead className={styles.thead}>
+          <tr className={styles.tr}>
+            <th className={styles.th}>{head1}</th>
+            <th className={styles.th}>{head2}</th>
+            <th className={styles.th}>{head3}</th>
+          </tr>
+        </thead>
+        <tbody className={styles.tbody}>
+          {
+            type === 'credited' ? (
+              credited().map((transaction:ITransaction) => (
+                <tr className={styles.tr} key={transaction.id }>
+                  <td className={styles.td}>{`@${transaction.debitedAccount.user.username}`}</td>
+                  <td className={styles.td}>{`R$${convertValue(transaction.value)}`}</td>
+                  <td className={styles.td}>{convertDate(transaction.createdAt)}</td>
+                </tr>
+              ))
+            ) : (
+              debited().map((transaction:ITransaction) => (
+                <tr className={styles.tr} key={transaction.id}>
+                  <td className={styles.td}>{`@${transaction.creditedAccount.user.username}`}</td>
+                  <td className={styles.td}>{`R$${convertValue(transaction.value)}`}</td>
+                  <td className={styles.td}>{convertDate(transaction.createdAt)}</td>
+                </tr>
+              ))
+            )
+          }
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TransactionTable;
