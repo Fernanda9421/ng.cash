400488b6421b3e4d0e464013afe80bf7d5009b32
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-11-24 09:33:07 +0000

    feat:  adiciona nome de usuário na área de transferências

--- app/ngcash_frontend/src/components/Transfer/index.tsx
+++ app/ngcash_frontend/src/components/Transfer/index.tsx
@@ -1,12 +1,21 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect, useState } from 'react';
 import styles from '@/styles/Transfer/transfer.module.css';
 import FormTransfer from './FormTransfer';
+import { storageGetItem } from 'src/utils/localStorage';
 
 const Transfer: FunctionComponent = () => {
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    const { username } = storageGetItem('user');
+    setUser(username);
+  }, []);
+
   return (
     <div>
       <div className={styles.presentationContainer}>
         <h1 className={styles.title}>Área de transferência</h1>
+        <p className='text-secondary'>@{user}</p>
       </div>
       <FormTransfer />
     </div>
