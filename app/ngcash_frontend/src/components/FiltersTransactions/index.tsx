2c4d4a7f4f44b67a4366746b02f1f354b63a20b2
Fernanda Andrade de Carvalho <fer.candrade21@gmail.com>
2022-11-25 17:34:13 +0000

    feat: transações por data

--- app/ngcash_frontend/src/components/FiltersTransactions/index.tsx
+++ app/ngcash_frontend/src/components/FiltersTransactions/index.tsx
@@ -3,21 +3,17 @@ import { FunctionComponent, useContext, useEffect, useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { AppContext } from '../../context/AppContext';
-import { Exception } from '../../interfaces/error';
-import { requestGet, setToken } from '../../services/requests';
+import {  setToken } from '../../services/requests';
 import { storageGetItem } from '../../utils/localStorage';
 import Button from '../Button';
 import styles from '../../styles/FiltersTransactions/filtersTransactions.module.css';
+import { IType, IUser } from './interfaces';
+import { getTransactionsByDate } from '../../utils/getTransactionsByDate';
 
-type IUser = {
-  username: string,
-  id: number,
-}
-
-const FiltersTransactions = ({type}:any) => {
+const FiltersTransactions:FunctionComponent<IType> = ({type}) => {
   const [dateSelected, setDateSelected] = useState(new Date());
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const { setIsFiltered, setTransactions }:any = useContext(AppContext);
+  const { setIsFiltered, setTransactions } = useContext(AppContext);
   const [user, setUser] = useState({
     username: '',
     id: 0,
@@ -34,20 +30,6 @@ const FiltersTransactions = ({type}:any) => {
     setIsAuthenticated(true);
   }, [route]);
 
-  const getTransactionsByDate = async () => {
-    try {
-      const transactions = await
-      requestGet(`/transaction/${user.id}?createdAt=${dateSelected}&${type}=true`);
-      setTransactions(transactions);
-      setIsFiltered(true);
-      console.log(transactions);
-      
-    } catch (error) {
-      const result = (error as Exception).response.data.message;
-      console.log(result);
-    }
-  };
-
   return (
     <div className={styles.container}>
       <div className={styles.boxDateSelected}>
@@ -60,7 +42,10 @@ const FiltersTransactions = ({type}:any) => {
             onChange={ (date:Date) => setDateSelected(date) }
           />
         </div>
-        <Button className={styles.button} name='Transações por data' onClick={async () => await getTransactionsByDate()} />
+        <Button
+          className={styles.button}
+          name='Transações por data'
+          onClick={() => getTransactionsByDate({id: user.id, dateSelected, type, setTransactions, setIsFiltered})} />
       </div>
     </div>
   );
---
+++ app/ngcash_frontend/src/components/FiltersTransactions/interfaces/index.tsx
@@ -0,0 +1,8 @@
+export interface IUser {
+  username: string;
+  id: number;
+};
+
+export interface IType {
+  type: string;
+};
